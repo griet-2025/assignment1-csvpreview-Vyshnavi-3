8f4bc4f473b226d380e65684b5a33def88025ee5
vyshnavi <vyshnavikanakapudi31@gmail.com>
2025-07-27 17:31:45 +0530

    Vyshu

--- task1/CSVReaderPreview.java
+++ task1/CSVReaderPreview.java
@@ -7,8 +7,42 @@ import java.io.IOException;
 public class CSVReaderPreview {
 
     public static void main(String[] args) {
-        // TO DO: Write your code below 
         String filePath = "dataset/dataset.csv";
-        
+        String line = "";
+        String delimiter = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+            String headerLine = br.readLine();
+            if (headerLine == null) {
+                System.out.println("Empty CSV file...");
+                return;
+            }
+
+            System.out.println("======= Data Preview ======\n");
+
+            String[] columns = headerLine.split(delimiter);
+            System.out.println("Columns:");
+            for (String column : columns) {
+                System.out.print(column + " ");
+            }
+
+            System.out.println("\nTotal columns : " + columns.length);
+            System.out.println("\n\n First 5 Records:\n");
+
+            int count = 0;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(delimiter);
+                if (count < 5) {
+                    System.out.println(String.join(" ", data));
+                }
+                count++;
+            }
+
+            System.out.println("\n\nTotal Records : " + count);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
